66d891626e9cdd54ed3086b923869434f1e79dc8
Alcino Sousa <alcino.jssousa@gmail.com>
2018-12-16 18:55:20 +0000

    recipes list prototype

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,11 +8,16 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('This is a new recipe',
+    'This is the description of a new recipe',
+    'https://www.publicdomainpictures.net/pictures/30000/velka/cornucopia.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
-   
+
   }
 
 }
